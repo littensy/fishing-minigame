a61067d5b10001ae62acf6dd0721086b19217ce0
littensy <56808540+littensy@users.noreply.github.com>
2025-11-26 00:14:55 -0800

    refactor: use new type solver

--- bin/check.sh
+++ bin/check.sh
@@ -4,8 +4,18 @@ curl -o roblox.d.luau https://luau-lsp.pages.dev/globalTypes.None.d.luau
 
 rojo sourcemap --output sourcemap.json
 
+# NOTE: LuauNoScopeShallNotSubsumeAll is required for the analyzer to work with
+# the new solver.
 echo "Running luau-lsp analyze"
-luau-lsp analyze --sourcemap="sourcemap.json" --ignore="**/submodules/**" --base-luaurc=".luaurc" --definitions="roblox.d.luau" src
+luau-lsp analyze \
+	--sourcemap="sourcemap.json" \
+	--ignore="**/submodules/**" \
+	--base-luaurc=".luaurc" \
+	--definitions="roblox.d.luau" \
+	--no-flags-enabled \
+	--flag:"LuauSolverV2=true" \
+	--flag:"LuauNoScopeShallNotSubsumeAll=true" \
+	src
 
 echo "Running selene"
 selene src
